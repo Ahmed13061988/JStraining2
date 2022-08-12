794e4af30759d51d8c7638d9482403f28913037a
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-12 13:56:43 -0600

    Function declarations vs Function expression

--- script.js
+++ script.js
@@ -6,21 +6,37 @@
 // if (passTest) hasDriversLicense = true;
 // if (hasDriversLicense) console.log(`I can drive!`);
 
-function logger() {
-    console.log(`My name is Ahmed`);
+// function logger() {
+//     console.log(`My name is Ahmed`);
+// }
+// // calling, running, invoking the function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// function 
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+
 }
-// calling, running, invoking the function
-logger();
-logger();
-logger();
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
-    return juice;
+const age = calcAge1(1988);
+
+//function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+const age2 = calcAge2(1995);
+console.log(age, age2)
