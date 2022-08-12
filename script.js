7ab19ef6ba9f19a8aec09cb6ec2acd3e536b76e6
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-12 13:25:17 -0600

    functions

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log(`I can drive!`);
+
+function logger() {
+    console.log(`My name is Ahmed`);
+}
+// calling, running, invoking the function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
