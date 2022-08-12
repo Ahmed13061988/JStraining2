aff6648d15ff0fe3c22401f55c3789acaa6fdc2d
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-12 13:38:13 -0600

    invoke the function

--- script.js
+++ script.js
@@ -15,9 +15,12 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-    console.log(apples, oranges);
     const juice = `Juice with ${apples} apples and ${oranges} oranges`;
     return juice;
 }
 
-fruitProcessor(5, 0);
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
