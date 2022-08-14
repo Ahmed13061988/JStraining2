06808c6d6d307d969912aaa4c7ddd17cebe513ac
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-14 09:19:00 -0600

    simple object

---
+++ objects.js
@@ -0,0 +1,8 @@
+const ahmed = {
+    firstName: `Ahmed`,
+    lastName: `Hussein`,
+    age: 2022 - 1988,
+    job: `WebDeveloper`,
+    friends: [`Wael`, `Ahmed`, `Ayman`]
+}
+// the order of values not matter at all when we need to retrive them.
