d79111b344adc7a0ef0611e81b7f51cbc33f1295
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-14 09:44:00 -0600

    retriving data from object

--- objects.js
+++ objects.js
@@ -2,7 +2,18 @@ const ahmed = {
     firstName: `Ahmed`,
     lastName: `Hussein`,
     age: 2022 - 1988,
-    job: `WebDeveloper`,
+    job: `Web Developer`,
     friends: [`Wael`, `Ahmed`, `Ayman`]
 }
 // the order of values not matter at all when we need to retrive them.
+console.log(ahmed);
+console.log(ahmed.firstName); // dot notation calling 
+console.log(ahmed[`lastName`]); // bracket notation calling
+const nameKey = `Name`;
+console.log(ahmed[`first` + nameKey]);
+console.log(ahmed[`last` + nameKey]); // example how we can pass an expression to the squar brackets 
+const intrestedIn = prompt(`What do you want to know about Ahmed? choose among first name, last name, age, job and friends`);
+console.log(ahmed[intrestedIn] ? ahmed[intrestedIn] : `Incorrect input!!`);
+
+
+
