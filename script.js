083863ddf42333e628cafb3783208097431e2051
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-13 20:07:07 -0600

    another approach for solving the code challenge

--- script.js
+++ script.js
@@ -122,6 +122,11 @@ function calcAge(birthYear) {
 
 const years = [1990, 1967, 2002, 2010, 2018];
 
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
 const newYears = [];
 for (let i = 0; i < years.length; i++) {
     const x = calcAge(years[i]);
