159b086f7532b24db7b39f5814f6df96fef9ae2f
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-14 09:51:57 -0600

    small challenge

--- objects.js
+++ objects.js
@@ -12,8 +12,15 @@ console.log(ahmed[`lastName`]); // bracket notation calling
 const nameKey = `Name`;
 console.log(ahmed[`first` + nameKey]);
 console.log(ahmed[`last` + nameKey]); // example how we can pass an expression to the squar brackets 
-const intrestedIn = prompt(`What do you want to know about Ahmed? choose among first name, last name, age, job and friends`);
-console.log(ahmed[intrestedIn] ? ahmed[intrestedIn] : `Incorrect input!!`);
+// const intrestedIn = prompt(`What do you want to know about Ahmed? choose among first name, last name, age, job and friends`);
+// console.log(ahmed[intrestedIn] ? ahmed[intrestedIn] : `Incorrect input!!`);
+
+ahmed.location = `Denver`;
+ahmed[`facebook`] = `AhmedAliHussein`;
+
+const challenge = `${ahmed.firstName} has ${ahmed.friends.length} friends, and the best friend is called ${ahmed.friends[0]}`;
+
+console.log(challenge);
 
 
 
