b5b63f7f547019e3823e0ae600130dfa5c00aed6
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-14 09:59:31 -0600

    adding function as an elemnt to the object

--- objects.js
+++ objects.js
@@ -6,12 +6,12 @@ const ahmed = {
     friends: [`Wael`, `Ahmed`, `Ayman`]
 }
 // the order of values not matter at all when we need to retrive them.
-console.log(ahmed);
-console.log(ahmed.firstName); // dot notation calling 
-console.log(ahmed[`lastName`]); // bracket notation calling
+//console.log(ahmed);
+//console.log(ahmed.firstName); // dot notation calling 
+//console.log(ahmed[`lastName`]); // bracket notation calling
 const nameKey = `Name`;
-console.log(ahmed[`first` + nameKey]);
-console.log(ahmed[`last` + nameKey]); // example how we can pass an expression to the squar brackets 
+//console.log(ahmed[`first` + nameKey]);
+//console.log(ahmed[`last` + nameKey]); // example how we can pass an expression to the squar brackets 
 // const intrestedIn = prompt(`What do you want to know about Ahmed? choose among first name, last name, age, job and friends`);
 // console.log(ahmed[intrestedIn] ? ahmed[intrestedIn] : `Incorrect input!!`);
 
@@ -20,7 +20,20 @@ ahmed[`facebook`] = `AhmedAliHussein`;
 
 const challenge = `${ahmed.firstName} has ${ahmed.friends.length} friends, and the best friend is called ${ahmed.friends[0]}`;
 
-console.log(challenge);
+// adding a function as an element to the object.
+const ahmed1 = {
+    firstName: `Ahmed`,
+    lastName: `Hussein`,
+    birthYear: 1988,
+    job: `Web Developer`,
+    friends: [`Wael`, `Ahmed`, `Ayman`],
+    hasDriversLicense: true,
+
+    calcAge: function (birthYear) {
+        2022 - birthYear;
+    }
+}
+
 
 
 
