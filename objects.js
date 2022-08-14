18c4486902482ba72695f4150fb2d6ce2e538a1f
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-14 10:44:45 -0600

    code challenge

--- objects.js
+++ objects.js
@@ -7,8 +7,8 @@ const ahmed = {
 }
 // the order of values not matter at all when we need to retrive them.
 //console.log(ahmed);
-//console.log(ahmed.firstName); // dot notation calling 
-//console.log(ahmed[`lastName`]); // bracket notation calling
+ahmed.firstName; // dot notation calling 
+ahmed[`lastName`]; // bracket notation calling
 const nameKey = `Name`;
 //console.log(ahmed[`first` + nameKey]);
 //console.log(ahmed[`last` + nameKey]); // example how we can pass an expression to the squar brackets 
@@ -28,12 +28,41 @@ const ahmed1 = {
     job: `Web Developer`,
     friends: [`Wael`, `Ahmed`, `Ayman`],
     hasDriversLicense: true,
-    calcAge: function (birthYear) {
-        return 2022 - birthYear;
+    // calcAge: function (birthYear) {
+    //     return 2022 - birthYear;
+    // }
+    // calcAge: function () {
+    //     console.log(this) // this keyword will refer to the object itself
+    //     return 2022 - this.birthYear;
+    // }
+    calcAge: function () {
+        //console.log(this) // this keyword will refer to the object itself
+        this.age = 2022 - this.birthYear // here the this will point to the ahmed1 itself
+        return this.age;
     }
 }
-console.log(ahmed1.calcAge(1988));
-console.log(ahmed1[`calcAge`](1988)); //brackets calling for the element.
+// console.log(ahmed1.calcAge(ahmed1.birthYear));
+// console.log(ahmed1[`calcAge`](ahmed1[`birthYear`])); //brackets calling for the element.
+
+//another way to call the function from the object 
+// console.log(ahmed1.calcAge());
+
+// console.log(ahmed1.age);
+// console.log(ahmed1.age);
+// console.log(ahmed1.age);
+
+
+const challenge1 = ahmed1.hasDriversLicense ? `${ahmed1.firstName} is a ${ahmed1.calcAge()}-years old ${ahmed1.job}, and he has a driver's license` : `${ahmed1.firstName} is a ${ahmed1.calcAge()}-years old ${ahmed1.job}, and he has no driver's license`
+console.log(challenge1);
+
+// if (ahmed1.hasDriversLicense) {
+//     console.log(challenge1 + `, and he has a driver's license`);
+// } else {
+
+//     console.log(challenge1 + `, and he don't has a driver's license`);
+// }
+
+
 
 
 
