0be9be4e6a0bdad914e91bab96b5e9efbc398c6f
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-15 10:18:47 -0600

    looping over an array

--- forLoop.js
+++ forLoop.js
@@ -1,3 +1,13 @@
+//for loop will executed while the condition is true
 for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Number of the repeating: ${rep} `);
+    //console.log(`Number of the repeating: ${rep} `);
 }
+
+const ahmed = [`Ahmed`, `Hussein`, 2022 - 1988, `Web Developer`, [`Wael`, `Ahmed`, `Hassan`]];
+const types = [];
+for (let i = 0; i < ahmed.length; i++) {
+    types.push(typeof ahmed[i]);
+    console.log(ahmed[i], typeof ahmed[i]);
+
+}
+console.log(types);
