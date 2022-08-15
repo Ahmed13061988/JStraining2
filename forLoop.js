14a16ccd7847b5bd37060780d9425a944c712a3a
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-15 10:36:57 -0600

    adding elements from one array to another

--- forLoop.js
+++ forLoop.js
@@ -7,7 +7,16 @@ const ahmed = [`Ahmed`, `Hussein`, 2022 - 1988, `Web Developer`, [`Wael`, `Ahmed
 const types = [];
 for (let i = 0; i < ahmed.length; i++) {
     types.push(typeof ahmed[i]);
-    console.log(ahmed[i], typeof ahmed[i]);
+    //  console.log(ahmed[i], typeof ahmed[i]);
 
 }
-console.log(types);
+//console.log(types);
+
+const years = [1988, 2007, 1969, 1954];
+
+const ages = [];
+for (let i = 0; i < years.length; i++) {
+
+    ages.push(2022 - years[i]);
+}
+console.log(ages);
